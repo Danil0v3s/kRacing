bf2de04a8a05c44fa6e3b50e7a7dd03017f3530a
Danilo Lemes <daniloleemes@hotmail.com>
2024-09-19 21:30:12 +0100

    update revs with viewmodel

--- target/client/src/main/kotlin/ui/app/App.kt
+++ target/client/src/main/kotlin/ui/app/App.kt
@@ -2,40 +2,55 @@ package ui.app
 
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
 import iracing.IRacingData
-import iracing.telemetry.TelemetryData
+import iracing.RPM
 import iracing.yaml.SessionInfoData
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.filterIsInstance
+import kotlinx.coroutines.launch
 import led.LedsController
 import repository.GameDataRepository
 import ui.components.dashboards.MainDashboard
 
-@Composable
-fun App() = MaterialTheme {
-//    val filters = remember { listOf("RPM") }
-//    val telemetryData by GameDataRepository.telemetry(filters).collectAsState(IRacingData.Telemetry(telemetry = emptyMap()))
-//    val sessionData by GameDataRepository.session().collectAsState(null)
+class AppViewModel : ViewModel() {
+    val filters = remember { listOf("RPM") }
 
-    MainDashboard()
-}
+    init {
+        CoroutineScope(Dispatchers.IO).launch {
+            GameDataRepository
+                .telemetry(filters)
+                .filterIsInstance(IRacingData.Telemetry::class)
+                .collect {
+                    LedsController.updateRevs(it.RPM.toInt())
+                }
 
-private fun updateSession(session: SessionInfoData) {
-    session.DriverInfo?.also {
-        LedsController.setRevOptions(
-            it.DriverCarIdleRPM,
-            it.DriverCarSLFirstRPM,
-            it.DriverCarSLShiftRPM,
-            it.DriverCarSLLastRPM,
-            it.DriverCarSLBlinkRPM
-        )
-    } ?: LedsController.setIdle()
-}
-
-private fun updateRPM(data: Map<String, TelemetryData>) {
-    data["RPM"]?.value?.let {
-        LedsController.updateRevs(it.toFloatOrNull()?.toInt() ?: 0)
+            GameDataRepository
+                .session()
+                .filterIsInstance(IRacingData.Session::class)
+                .collect {
+                    updateSession(it.session)
+                }
+        }
     }
+
+    private fun updateSession(session: SessionInfoData) {
+        session.DriverInfo?.also {
+            LedsController.setRevOptions(
+                it.DriverCarIdleRPM,
+                it.DriverCarSLFirstRPM,
+                it.DriverCarSLShiftRPM,
+                it.DriverCarSLLastRPM,
+                it.DriverCarSLBlinkRPM
+            )
+        } ?: LedsController.setIdle()
+    }
+}
+
+@Composable
+fun App(viewModel: AppViewModel = viewModel()) = MaterialTheme {
+    MainDashboard()
 }
